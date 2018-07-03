7edbe508cf107f0ca4a016e6bee39c192202b199
Jay <tuna_sang@hotmail.com>
2018-07-03 11:53:52 +0700

    แก้ไข icon header

--- application/views/Front/themes/header.php
+++ application/views/Front/themes/header.php
@@ -15,7 +15,7 @@
   <!-- Google-code-prettify -->
   <link href="<?php echo BASE_URL()?>/assets/Front/themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
   <!-- fav and touch icons -->
-  <link rel="shortcut icon" href="<?php echo BASE_URL()?>/assets/Front/themes/images/ico/favicon.ico">
+  <link rel="shortcut icon" href="<?php echo BASE_URL()?>/assets/Front/themes/images/ico/newicon_ZV1_icon.ico">
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo BASE_URL()?>/assets/Front/themes/images/ico/apple-touch-icon-144-precomposed.png">
   <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo BASE_URL()?>/assets/Front/themes/images/ico/apple-touch-icon-114-precomposed.png">
   <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo BASE_URL()?>/assets/Front/themes/images/ico/apple-touch-icon-72-precomposed.png">
